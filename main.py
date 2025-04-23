510365d3006700ace92955b9b33850d9b57a5090
andynic <andysnic@hotmail.com>
2025-04-23 15:40:28 +0300

    not tested

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import os
 import random
 from PyQt6 import QtWidgets, QtCore, QtGui
+from PIL import Image, ExifTags
+from geopy.geocoders import Nominatim  # for reverse geocoding metadata
 
 IMAGE_EXTENSIONS = ('.jpg', '.jpeg', '.png', '.bmp', '.gif')
 
@@ -19,6 +21,55 @@ def scan_images(folder_path):
     return image_paths
 
 
+def extract_metadata(image_path, geolocator, cache):
+    """
+    Extracts date and location (city, country) from image EXIF metadata.
+    Caches results to speed up repeated calls.
+    Returns tuple (date_str or '', location_str or '').
+    """
+    if image_path in cache:
+        return cache[image_path]
+
+    date_str = ''
+    location_str = ''
+
+    try:
+        img = Image.open(image_path)
+        exif_data = img._getexif() or {}
+        # Map tag IDs to names
+        exif = {ExifTags.TAGS.get(k, k): v for k, v in exif_data.items()}
+
+        # Date
+        if 'DateTimeOriginal' in exif:
+            date_str = exif['DateTimeOriginal'].split(' ')[0].replace(':', '-')
+
+        # GPS
+        if 'GPSInfo' in exif:
+            gps = exif['GPSInfo']
+            # Helper to convert coords
+            def _convert(coord, ref):
+                d, m, s = coord
+                dec = d[0]/d[1] + m[0]/m[1]/60 + s[0]/s[1]/3600
+                return -dec if ref in ['S', 'W'] else dec
+
+            lat = _convert(gps[2], gps[1]) if 1 in gps and 2 in gps else None
+            lon = _convert(gps[4], gps[3]) if 3 in gps and 4 in gps else None
+
+            if lat and lon:
+                # Reverse geocode
+                loc = geolocator.reverse((lat, lon), exactly_one=True, language='en')
+                if loc and 'address' in loc.raw:
+                    addr = loc.raw['address']
+                    city = addr.get('city') or addr.get('town') or addr.get('village') or ''
+                    country = addr.get('country', '')
+                    location_str = ', '.join(filter(None, [city, country]))
+    except Exception:
+        pass
+
+    cache[image_path] = (date_str, location_str)
+    return cache[image_path]
+
+
 class SlideshowWindow(QtWidgets.QWidget):
     def __init__(self, images, interval, parent=None):
         super().__init__(parent)
@@ -26,39 +77,50 @@ class SlideshowWindow(QtWidgets.QWidget):
         self.interval_ms = int(interval * 1000)
         self.current_index = 0
 
-        # Ensure widget is deleted on close so MainWindow can re-show
-        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_DeleteOnClose)
         # Black background
         self.setStyleSheet("background-color: black;")
-
-        # Fullscreen & borderless
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_DeleteOnClose)
         self.setWindowFlags(QtCore.Qt.WindowType.FramelessWindowHint)
         self.showFullScreen()
 
-        # Label for image display
+        # Prepare geolocator and cache
+        self.geolocator = Nominatim(user_agent="digital_photo_frame")
+        self.metadata_cache = {}
+
+        # Image label
         self.label = QtWidgets.QLabel(self)
         self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.label.setStyleSheet("background-color: black;")
 
+        # Info label (date & location)
+        self.info_label = QtWidgets.QLabel(self)
+        self.info_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        self.info_label.setStyleSheet(
+            "color: white; background-color: rgba(0, 0, 0, 128); padding: 5px;"
+        )
+
+        # Layout
         layout = QtWidgets.QVBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.label)
+        layout.addWidget(self.label, stretch=1)
+        layout.addWidget(self.info_label)
 
-        # Timer for image changes
+        # Timer
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.show_next_image)
         self.timer.start(self.interval_ms)
 
-        # Show initial image
         self.show_image()
 
     def show_image(self):
         if not self.images:
             return
-        img_path = self.images[self.current_index]
-        pixmap = QtGui.QPixmap(img_path)
+        path = self.images[self.current_index]
+        pixmap = QtGui.QPixmap(path)
         if pixmap.isNull():
             return
+
+        # Scale to screen size
         screen_rect = QtWidgets.QApplication.primaryScreen().geometry()
         pixmap = pixmap.scaled(
             screen_rect.size(),
@@ -67,15 +129,16 @@ class SlideshowWindow(QtWidgets.QWidget):
         )
         self.label.setPixmap(pixmap)
 
+        # Metadata
+        date_str, location_str = extract_metadata(path, self.geolocator, self.metadata_cache)
+        info = ' | '.join(filter(None, [date_str, location_str]))
+        self.info_label.setText(info)
+
     def show_next_image(self):
-        if not self.images:
-            return
         self.current_index = (self.current_index + 1) % len(self.images)
         self.show_image()
 
     def show_prev_image(self):
-        if not self.images:
-            return
         self.current_index = (self.current_index - 1) % len(self.images)
         self.show_image()
 
@@ -89,11 +152,9 @@ class SlideshowWindow(QtWidgets.QWidget):
             self.close()
 
     def mousePressEvent(self, event):
-        # Any mouse click exits slideshow
         self.close()
 
     def closeEvent(self, event):
-        # Stop the timer and accept close
         self.timer.stop()
         event.accept()
         super().closeEvent(event)
@@ -137,7 +198,6 @@ class MainWindow(QtWidgets.QWidget):
         if folder:
             self.folder_label.setText(folder)
             self.image_list = scan_images(folder)
-            # Shuffle images for random order
             random.shuffle(self.image_list)
             self.count_label.setText(f"{len(self.image_list)} Images found.")
 
@@ -147,7 +207,6 @@ class MainWindow(QtWidgets.QWidget):
             return
         interval = self.interval_spin.value()
         self.hide()
-        # Keep a reference to prevent GC
         self.slideshow = SlideshowWindow(self.image_list, interval)
         self.slideshow.destroyed.connect(self.show)
         self.slideshow.show()
